eeccd7170939c1dbc9c559d87668813f002ef3c8
athena-galaxies <averyspence117@gmail.com>
2025-06-27 12:03:04 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,37 +1,36 @@
-Hooks.once("init", async function () {
-  console.log("Dreambrew | Initializing Dreambrew Character Sheet");
+console.log("Dreambrew sheet loaded!");
 
-  // Register the custom actor sheet for dnd5e system
-  Actors.unregisterSheet("dnd5e", ActorSheet5eCharacter);
-  Actors.registerSheet("dreambrew", DreambrewActorSheet, {
-    types: ["character"],
-    makeDefault: true
-  });
-});
-
-// Define your custom actor sheet class
-class DreambrewActorSheet extends ActorSheet5eCharacter {
-  /** @override */
+class DreambrewActorSheet extends dnd5e.applications.actor.ActorSheet5eCharacter {
   static get defaultOptions() {
     return mergeObject(super.defaultOptions, {
-      classes: ["dreambrew", "sheet", "actor", "character"],
-      template: "modules/dreambrew-actor-sheet/templates/dreambrew-sheet.html",
+      classes: ["dnd5e", "sheet", "actor", "character", "dreambrew"],
+      template: "templates/sheet.html",
       width: 800,
       height: 700,
       tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description" }]
     });
   }
 
-  /** @override */
   getData() {
     const data = super.getData();
-    // You can manipulate or add custom data here if needed
+    // Put custom logic here if needed
     return data;
   }
 
-  /** @override */
   activateListeners(html) {
     super.activateListeners(html);
-    // Add custom listeners here
+    // Add your interactive events here if needed
   }
 }
+
+Hooks.once("init", () => {
+  console.log("Initializing Dreambrew Character Sheet");
+
+  // Register the new sheet
+  Actors.unregisterSheet("dnd5e", dnd5e.applications.actor.ActorSheet5eCharacter);
+  Actors.registerSheet("dnd5e", DreambrewActorSheet, {
+    types: ["character"],
+    makeDefault: true,
+    label: "Dreambrew Sheet"
+  });
+});
