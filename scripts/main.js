d05abb0fa866d1d60f3506d27373320c203cb92b
athena-galaxies <averyspence117@gmail.com>
2025-06-27 11:51:26 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,28 @@
-console.log("Dreambrew sheet loaded!");
+class DreambrewActorSheet extends ActorSheet5eCharacter {
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      classes: ["dreambrew", "sheet", "actor"],
+      template: "templates/sheet.html", // Adjust if your file path is different
+      width: 800,
+      height: 700,
+      tabs: [
+        {
+          navSelector: ".sheet-tabs",
+          contentSelector: ".sheet-body",
+          initial: "main"
+        }
+      ]
+    });
+  }
+}
 
-Hooks.once("init", function () {
-  console.log("Initializing Dreambrew Character Sheet");
+Hooks.once("init", () => {
+  console.log("Dreambrew | Registering custom actor sheet");
 
-  Actors.registerSheet("dnd5e", DreambrewActorSheet, {
+  Actors.unregisterSheet("core", ActorSheet5eCharacter);
+  Actors.registerSheet("dreambrew", DreambrewActorSheet, {
     types: ["character"],
-    makeDefault: false,
+    makeDefault: true,
     label: "Dreambrew Sheet"
   });
 });
